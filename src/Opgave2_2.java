fbabcee5614c866b609e6a045b78c3e610abee60
jenna-P <jenn2464@edu.easj.dk>
2020-09-14 19:00:35 +0200

    commit

--- src/Opgave2_2.java
+++ src/Opgave2_2.java
@@ -4,11 +4,12 @@ public class Opgave2_2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter length of sides and height of the Equilateral triangle : ");
+        System.out.print("Enter length of sides ");
+        System.out.println("and height of the Equilateral triangle : ");
         double lengthOfSides = input.nextDouble();
-        double height = input.nextDouble();  //dnt know this part
+        double height = input.nextDouble();  
 
-        double area = (Math.sqrt(3) / 4) * lengthOfSides;
+        double area = (Math.sqrt(3) / 4) * lengthOfSides * lengthOfSides;
         double volume = area * height;
 
         System.out.println("The area is " + area);
