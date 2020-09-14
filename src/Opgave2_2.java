afecec0b89542044959856dfc68549192ca481b2
jenna-P <jenn2464@edu.easj.dk>
2020-09-14 19:11:54 +0200

    commit

--- src/Opgave2_2.java
+++ src/Opgave2_2.java
@@ -7,7 +7,7 @@ public class Opgave2_2 {
         System.out.print("Enter length of sides ");
         System.out.println("and height of the Equilateral triangle : ");
         double lengthOfSides = input.nextDouble();
-        double height = input.nextDouble();  
+        double height = input.nextDouble();
 
         double area = (Math.sqrt(3) / 4) * lengthOfSides * lengthOfSides;
         double volume = area * height;
