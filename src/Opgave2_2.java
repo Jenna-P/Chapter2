5f24926f2a81b531672cad32595139cac7c7ca12
jenna-P <jenn2464@edu.easj.dk>
2020-09-14 18:25:44 +0200

    commit

--- src/Opgave2_2.java
+++ src/Opgave2_2.java
@@ -4,11 +4,12 @@ public class Opgave2_2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter length of sides of the Equilateral triangle : ");
+        System.out.print("Enter length of sides and height of the Equilateral triangle : ");
         double lengthOfSides = input.nextDouble();
+        double height = input.nextDouble();  //dnt know this part
 
         double area = (Math.sqrt(3) / 4) * lengthOfSides;
-        double volume = area * lengthOfSides; // ved ikke length og lengthofside er samme
+        double volume = area * height;
 
         System.out.println("The area is " + area);
         System.out.println("The volume of the trianrular is " + volume);
